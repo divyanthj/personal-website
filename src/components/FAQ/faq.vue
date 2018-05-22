4d333e7981c0fb2832c796f1f4f4cf9f5ca768a1
Divyanth Jayaraj <divyanthj@gmail.com>
2018-05-22 17:04:57 +0530

    Added FAQ section

--- src/components/FAQ/faq.vue
+++ src/components/FAQ/faq.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-
+  <div class='faq'>
+    <div class='ui accordion'>
+      <div v-for='question in questions'>
+        <div class='title'>
+          {{question.q}}
+        </div>
+        <div class='content'>
+          <p v-for='paragraph in question.a'>
+            {{paragraph}}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import FAQ from '../../content/faq'
 export default {
-
+  mounted() {
+    console.log("UI has been updated");
+    $('.ui.accordion')
+        .accordion()
+  },
+  data() {
+    return {
+      questions: FAQ
+    }
+  }
 }
 </script>
 <style>
+.faq {
+  padding: 3em;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  background-color: white;
+  box-shadow: 1px 1px 5px #aaaaaa;
+  border-radius: 10px;
+  height: 85%;
+}
 </style>
---
+++ src/content/faq.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    q: "What motivates you as a programmer?",
+    a: [
+      "I like to build things. I really enjoy how the effort I put in the form of code, materializes in front of me as an end-product. My work, holding value with the stakeholders is an added bonus.",
+      "At the end of the day, I could point at my work and say 'I made it'. The feeling that follows is undescribable."
+    ]
+  },
+  {
+    q: "What else are you interested in besides programming?",
+    a: [
+      "I like playing video games, especially strategy games where you get to build an empire. I like the idea of starting small and then scaling it up. So, I emulate it wherever I can.",
+      "Besides video games, I am passionate about Blockchain and Cryptocurrencies. In fact, expertise in blockchain technologies is one of the most significant items on my bucket list."
+    ]
+  }
+]
