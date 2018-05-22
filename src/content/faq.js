7a6e75908f7fd7eb4954e07988d1086c9d14b74e
Divyanth Jayaraj <divyanthj@gmail.com>
2018-05-22 21:17:45 +0530

    Added some questions

--- src/content/faq.js
+++ src/content/faq.js
@@ -1,9 +1,22 @@
 export default [
   {
+    q: "What do you do?",
+    a: [
+      "I am primarily a UI developer. I build rich web-based user interfaces for both customer-facing and internal applications."
+    ]
+  },
+  {
     q: "What motivates you as a programmer?",
     a: [
       "I like to build things. I really enjoy how the effort I put in the form of code, materializes in front of me as an end-product. My work, holding value with the stakeholders is an added bonus.",
       "At the end of the day, I could point at my work and say 'I made it'. The feeling that follows is the best!"
+    ]
+  },
+  {
+    q: "What is your personal philosophy when it comes to building user interfaces?",
+    a: [
+      "These days, presenting information is easy. Adding bells and whistles, is easy too.",
+      "The hard part, is to delve into your user-base's mind and figure out exactly how much information they need to see at any given time. Even though I struggle with figuring this out, I have realized that adopting a 'less is more' philosophy works best. Give the users only what they need."
     ]
   },
   {
