00c9dbf1024429f0f587ae3fcb4ece7ed4c67587
Divyanth Jayaraj <divyanthj@gmail.com>
2018-05-23 13:26:26 +0530

    Added link to source code

--- src/components/introduction/introduction.vue
+++ src/components/introduction/introduction.vue
@@ -7,7 +7,10 @@
       I have worked as a UI developer consultant for various clients across the United States. I have more than four years of experience building highly interactive web applications and user interfaces.
     </p>
     <p>
-      Go ahead and navigate through the website to learn more about me.
+      This website was built from scratch. It serves as a testament to my skill as a software developer.
+    </p>
+    <p>
+      Go ahead and navigate through the website to learn more about me. You can find its source code <a href='https://github.com/divyanthj/personal-website' target='_blank'>here</a>
     </p>
   </div>
 </template>
